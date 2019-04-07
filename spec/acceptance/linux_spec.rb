f4fc60f96ca5d8c2bd420efa3fadfa449ac9431b
David Newton <davidnewton82@gmail.com>
2019-04-07 19:49:07 +0000

    Update linux_spec.rb

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -8,7 +8,7 @@ describe 'linux package task', unless: os[:family] == 'windows' do
   windows = os[:family] == 'windows'
 
   describe 'serverspec' do
-    it 'executes serverspec', unless: || windows do
+    it 'executes serverspec', unless: windows do
       #apply_manifest_on(default, "file { 'rsyslog': ensure => absent, }")
       result = task_run('test::role', '', '', '',  "test_tool" => "serverspec" , "test_file" => "web_server.rb" )
       expect(result[0]['status']).to eq('success')
