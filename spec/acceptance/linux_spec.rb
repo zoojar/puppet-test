1d016dc7e9a7fddf6ef637904d77b9441409a921
davidjnewton <davidnewton82@gmail.com>
2019-04-08 23:18:01 +0100

    spec linux

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -1,7 +1,7 @@
 # run a test task
 require 'spec_helper_acceptance'
 
-describe 'linux package task', unless: os[:family] == 'windows' do
+describe 'test::role task', unless: os[:family] == 'windows' do
   include Beaker::TaskHelper::Inventory
   include BoltSpec::Run
 
@@ -11,7 +11,7 @@ describe 'linux package task', unless: os[:family] == 'windows' do
     it 'executes serverspec', unless: windows do
       # apply_manifest_on(default, "file { 'rsyslog': ensure => absent, }")
       result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
-      expect(result[0]['status']).to eq('success')
+      expect(result[0]).to eq('success')
     end
   end
 end
