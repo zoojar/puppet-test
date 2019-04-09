82b71f6e18da94ebc1699c0904702de4025e5b4f
davidjnewton <davidnewton82@gmail.com>
2019-04-09 18:42:15 +0100

    specs

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -7,21 +7,25 @@ describe 'test::role task', unless: os[:family] == 'windows' do
 
   windows = os[:family] == 'windows'
 
-  describe 'test tool serverspec, with test_file supplied' do
-    it 'runs a passing test successfully and returns output', unless: windows do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
-      expect(task_result[0]['status']).to eq('success')
-      expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
-    end
+  describe 'test_tool=serverspec, test_file=example_pass.rb' do
+    task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
     it 'installs serverspec gem', unless: windows do
       expect(shell('ls /tmp/puppet_test/serverspec/gems/serverspec*/serverspec.gemspec').exit_code).to eq(0)
     end
+    it 'runs a passing test successfully', unless: windows do
+      expect(task_result[0]['status']).to eq('success')
+    end
+    it 'returns output', unless: windows do
+      expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
+    end
   end
 
-  describe 'test tool serverspec, with no test_file supplied' do
-    it 'fails to run and returns helpful output', unless: windows do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec')
+  describe 'test_tool=serverspec' do
+    task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec')
+    it 'fails to run', unless: windows do
       expect(task_result[0]['status']).to eq('failure')
+    end
+    it 'returns helpful output', unless: windows do
       expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
     end
   end
