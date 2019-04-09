a8e139d5c6bcafbdfe267304c01ca5a6703c84cc
davidjnewton <davidnewton82@gmail.com>
2019-04-09 18:52:19 +0100

    spec, global for task result

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -9,24 +9,24 @@ describe 'test::role task', unless: os[:family] == 'windows' do
 
   describe 'test_tool=serverspec, test_file=example_pass.rb' do
     it 'installs serverspec gem', unless: windows do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
+      $task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
       expect(shell('ls /tmp/puppet_test/serverspec/gems/serverspec*/serverspec.gemspec').exit_code).to eq(0)
     end
     it 'runs a passing test successfully', unless: windows do
-      expect(task_result[0]['status']).to eq('success')
+      expect($task_result[0]['status']).to eq('success')
     end
     it 'returns output', unless: windows do
-      expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
+      expect($task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
     end
   end
 
   describe 'test_tool=serverspec' do
     it 'fails to run', unless: windows do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec')
-      expect(task_result[0]['status']).to eq('failure')
+      $task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec')
+      expect($task_result[0]['status']).to eq('failure')
     end
     it 'returns helpful output', unless: windows do
-      expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
+      expect($task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
     end
   end
   
