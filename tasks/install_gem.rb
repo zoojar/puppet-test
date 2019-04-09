9353e16fa72f59a6a3b53f03e9a96fec2cf35116
davidjnewton <davidnewton82@gmail.com>
2019-04-09 02:20:09 +0100

    install gem exitcode

--- tasks/install_gem.rb
+++ tasks/install_gem.rb
@@ -18,22 +18,30 @@ def install_gem(gem_bin, gem, version, install_dir)
     '-v', version
   ].shelljoin
   stdout, stderr, exitcode = Open3.capture3(cmd)
-  raise "Failed to install gem #{gem} using cmd: #{cmd} : #{stderr} #{stdout}" unless exitcode.zero?
+  if exitcode == 0
+    puts "gem #{gem} installed at #{install_dir}"
+  else
+    raise "Failed to install gem #{gem} using cmd: #{cmd} : #{stderr} #{stdout}"
+  end
 end
 
 begin
-  params = JSON.parse(STDIN.read)
-  os_tmp = case Facter.value(:kernel)
-           when 'Darwin' then '/Users/Shared/tmp'
-           when 'Linux' then '/tmp'
-           when 'Windows' then 'c:/tmp'
-           end
-  install_dir = params['install_dir'] ||= File.join(os_tmp, 'puppet_test')
-  gem_bin     = params['gem_bin'] ||= File.join('/opt', 'puppetlabs', 'puppet', 'bin', 'gem')
-  gem         = params['gem']
-  version     = params['version'] ||= '> 0' # latest
-  install_gem(gem_bin, gem, version, install_dir)
+  stdin = STDIN.read
+  unless stdin.empty?
+    params = JSON.parse(stdin)
+    os_tmp = case Facter.value(:kernel)
+             when 'Darwin' then '/Users/Shared/tmp'
+             when 'Linux' then '/tmp'
+             when 'Windows' then 'c:/tmp'
+             end
+    install_dir = params['install_dir'] ||= File.join(os_tmp, 'puppet_test')
+    gem_bin     = params['gem_bin'] ||= File.join('/opt', 'puppetlabs', 'puppet', 'bin', 'gem')
+    gem         = params['gem']
+    version     = params['version'] ||= '> 0' # latest
+    install_gem(gem_bin, gem, version, install_dir)
+  end
 rescue Puppet::Error => e
   # handle failure and exit
-  puts({ status: 'Notice', error: e.message }.to_json)
+  puts({ status: 'failure', error: e.message }.to_json)
+  exit 1
 end
