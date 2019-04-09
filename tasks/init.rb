87c2e2bd93ac563253bfb59da3191e2cb55b4216
davidjnewton <davidnewton82@gmail.com>
2019-04-09 18:54:14 +0100

    .to_s string

--- tasks/init.rb
+++ tasks/init.rb
@@ -38,9 +38,9 @@ def build_test_file_path(test_files_dir, test_tool, test_file, role)
   # Returns a file path based on the test_tool used and role or test_file specified,
   # if no role or test_file is specifed then we try to determine the target node's role.
   test_file = "#{role}.rb" unless role.nil?
-  if "#{test_file}".empty?
+  if test_file.to_s.empty?
     facter_role = "#{Facter.value(':role')}"
-    if "#{facter_role}".strip.empty?
+    if facter_role.to_s.strip.empty?
       raise [
         '# Unable to detect this node\'s role using facter.',
         '# You could try speficying a test_file as a parameter to this task.',
