1f4aa6ea5a58e82cf8088e675e60b130d8edcc9d
davidjnewton <davidnewton82@gmail.com>
2019-04-11 21:43:27 +0200

    plan spec

--- spec/acceptance/plan_minitest_spec.rb
+++ spec/acceptance/plan_minitest_spec.rb
@@ -9,14 +9,7 @@ describe 'plan test::role with minitest' do
   include BoltSpec::Run
   describe 'test_tool=minitest' do
     it 'fails to run' do
-      plan_result = plan_run('test::role', '', '', '', 
-        "target" => "webserver.local", "test_params" => {
-          "test_tool" => "serverspec",  "test_file" => "webserver.rb"
-        },
-        "ctrl_params" => {
-          "tmp_dir" => "/Users/Shared/tmp"
-        }
-      })
+      plan_result = plan_run('test::role', '', '', '', "target" => "webserver.local", "test_params" => {"test_tool" => "serverspec",  "test_file" => "webserver.rb" }, "ctrl_params" => { "tmp_dir" => "/Users/Shared/tmp" })
       expect(plan_result[0]['status']).to eq('failure')
     end
     it 'returns helpful error message' do
