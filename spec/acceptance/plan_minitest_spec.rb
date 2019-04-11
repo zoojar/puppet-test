21d061d621f1ab5790561a79c6f2adcba9879e28
davidjnewton <davidnewton82@gmail.com>
2019-04-11 23:33:06 +0200

    minitest specs

--- spec/acceptance/plan_minitest_spec.rb
+++ spec/acceptance/plan_minitest_spec.rb
@@ -9,11 +9,11 @@ describe 'plan test::role with minitest' do
   include BoltSpec::Run
   describe 'test_tool=minitest' do
     it 'fails to run' do
-      plan_result = run_plan('test::role', {"target" => "webserver.local", "test_params" => {"test_tool" => "serverspec",  "test_file" => "webserver.rb" }, "ctrl_params" => { "tmp_dir" => "/Users/Shared/tmp" }})
+      plan_result = run_plan('test::role', {'target'=> 'webserver.local', 'test_params'=> {'test_tool'=> 'serverspec',  'test_file'=> 'webserver.rb'}, 'ctrl_params'=> { 'tmp_dir'=> '/Users/Shared/tmp'}})
       expect(plan_result[0]['status']).to eq('failure')
     end
     it 'returns helpful error message' do
-      plan_result = run_plan('test::role', '', '', '', "target" => "webserver.local", "test_params" => {"test_tool" => "serverspec" }, "ctrl_params" => { "tmp_dir" => "/Users/Shared/tmp" })
+      plan_result = run_plan('test::role', {'target'=> 'webserver.local', 'test_params'=> {'test_tool'=> 'serverspec'}, 'ctrl_params'=> { 'tmp_dir'=> '/Users/Shared/tmp'}})
       expect(plan_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
     end
   end
