4e4ede10efa55c7596904b003ab8714f5e35c922
davidjnewton <davidnewton82@gmail.com>
2019-04-12 16:14:21 +0200

    plan spec

--- spec/acceptance/plan_minitest_spec.rb
+++ spec/acceptance/plan_minitest_spec.rb
@@ -13,14 +13,14 @@ describe 'plan test::role with minitest' do
                              'target'      => 'webserver.local',
                              'test_params' => { 'test_tool' => 'serverspec', 'test_file' => 'webserver.rb' },
                              'ctrl_params' => { 'tmp_dir' => '/Users/Shared/tmp' })
-      expect(plan_result[0]['status']).to eq('failure')
+      expect(plan_result['status']).to eq('failure')
     end
     it 'returns helpful error message' do
       plan_result = run_plan('test::role',
                              'target'      => 'webserver.local',
                              'test_params' => { 'test_tool' => 'serverspec' },
                              'ctrl_params' => { 'tmp_dir' => '/Users/Shared/tmp' })
-      expect(plan_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
+      expect(plan_result['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
     end
   end
 end
