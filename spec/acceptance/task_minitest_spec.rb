ae5830587b7bf1723b66af841cf3bbaff5145ec1
davidjnewton <davidnewton82@gmail.com>
2019-04-24 11:25:15 +0100

    rm  run_task helper run_task

--- spec/acceptance/task_minitest_spec.rb
+++ spec/acceptance/task_minitest_spec.rb
@@ -5,40 +5,40 @@ describe 'test::role with minitest' do
   include BoltSpec::Run
   describe 'test_tool=minitest' do
     it 'fails to run' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'minitest')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'minitest')
       expect(task_result[0]['status']).to eq('failure')
     end
     it 'returns helpful error message' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'minitest')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'minitest')
       expect(task_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
     end
   end
 
   describe 'test_tool=minitest, test_file=example_pass.rb' do
     it 'runs a passing test successfully' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_pass.rb')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_pass.rb')
       expect(task_result[0]['status']).to eq('success')
     end
     it 'returns output' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_pass.rb')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_pass.rb')
       expect(task_result[0]['result']['_output']).to match(%r{2\sruns,\s2\sassertions,\s0\sfailures,\s0\serrors,\s0\sskips})
     end
   end
 
   describe 'test_tool=minitest, test_file=example_pass.rb, return_status=false' do
     it 'does not return the status from the test' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_fail.rb', 'return_status' => false)
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_fail.rb', 'return_status' => false)
       expect(task_result[0]['status']).to eq('success')
     end
   end
 
   describe 'test_tool=minitest, test_file=example_pass.rb, return_status=true' do
     it 'returns status from test - success' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_pass.rb', 'return_status' => true)
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_pass.rb', 'return_status' => true)
       expect(task_result[0]['status']).to eq('success')
     end
     it 'returns status from test - failure' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_fail.rb', 'return_status' => true)
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'minitest', 'test_file' => 'example_fail.rb', 'return_status' => true)
       expect(task_result[0]['status']).to eq('failure')
     end
   end
--- spec/acceptance/task_serverspec_spec.rb
+++ spec/acceptance/task_serverspec_spec.rb
@@ -5,44 +5,44 @@ describe 'test::role with serverspec' do
   include BoltSpec::Run
   describe 'test_tool=serverspec' do
     it 'fails to run' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'serverspec')
       expect(task_result[0]['status']).to eq('failure')
     end
     it 'returns helpful error message' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'serverspec')
       expect(task_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
     end
   end
 
   describe 'test_tool=serverspec, test_file=example_pass.rb' do
     it 'installs serverspec gem' do
-      task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
+      run_task('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
       expect(shell('ls /tmp/puppet_test/serverspec/gems/serverspec*/serverspec.gemspec').exit_code).to eq(0)
     end
     it 'runs a passing test successfully' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
       expect(task_result[0]['status']).to eq('success')
     end
     it 'returns output' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb')
       expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
     end
   end
 
   describe 'test_tool=serverspec, test_file=example_pass.rb, return_status=false' do
     it 'does not return the status from the test' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_fail.rb', 'return_status' => false)
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_fail.rb', 'return_status' => false)
       expect(task_result[0]['status']).to eq('success')
     end
   end
 
   describe 'test_tool=serverspec, test_file=example_pass.rb, return_status=true' do
     it 'returns status from test - success' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb', 'return_status' => true)
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb', 'return_status' => true)
       expect(task_result[0]['status']).to eq('success')
     end
     it 'returns status from test - failure' do
-      task_result = task_run('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_fail.rb', 'return_status' => true)
+      task_result = run_task('test::role', '', '', '', 'test_tool' => 'serverspec', 'test_file' => 'example_fail.rb', 'return_status' => true)
       expect(task_result[0]['status']).to eq('failure')
     end
   end
