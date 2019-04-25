4f8c52cb08dc6ac995c226d39abd211d7560cb9a
davidjnewton <davidnewton82@gmail.com>
2019-04-25 16:25:18 +0100

    run task

--- spec/acceptance/task_inspec_spec.rb
+++ spec/acceptance/task_inspec_spec.rb
@@ -6,31 +6,32 @@ describe 'test::role with inspec' do
     it 'fails to run' do
       # inspec requires build-essential for gem install
       shell('puppet resource package build-essential ensure=installed')
-      task_result = run_task('test::role', 'test_tool' => 'inspec')
+      task_result = task_run('test::role', 'test_tool' => 'inspec')
       expect(task_result[0]['status']).to eq('failure')
     end
   
     it 'returns helpful error message' do
-      task_result = run_task('test::role', 'test_tool' => 'inspec')
+      task_result = task_run('test::role', 'test_tool' => 'inspec')
       expect(task_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
     end
 
     it 'installs inspec gem' do
-      run_task('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
-      expect(shell('ls /tmp/puppet_test/inspec/gems/inspec*/inspec.gemspec').exit_code).to eq(0)
+      task_run('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
+      cmd_result = run_command('ls /tmp/puppet_test/inspec/gems/inspec*/inspec.gemspec')
+      expect(cmd_result[0]['status']).to eq('success')
     end
     it 'runs a passing test successfully' do
-      task_result = run_task('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
+      task_result = task_run('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
       expect(task_result[0]['status']).to eq('success')
     end
 
     it 'returns output' do
-      task_result = run_task('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
+      task_result = task_run('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
       expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
     end
 
     it 'fails a failing test' do
-      task_result = run_task('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_fail.rb')
+      task_result = task_run('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_fail.rb')
       expect(task_result[0]['status']).to eq('failure')
     end
   end
