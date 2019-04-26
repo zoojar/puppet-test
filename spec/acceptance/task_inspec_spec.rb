5bc460fcfd945c36bb5671152b129993bfa9354b
davidjnewton <davidnewton82@gmail.com>
2019-04-26 10:28:52 +0100

    inspec moved to plan (requires dev tools)

--- spec/acceptance/task_inspec_spec.rb
+++ spec/acceptance/task_inspec_spec.rb
@@ -4,13 +4,23 @@ require 'spec_helper_acceptance'
 describe 'test_tool=inspec' do
   # inspec requires build-essential / dev tools for gem install
   # so the plan is used for inspec - we build locally and copy to node.
-  it 'fails to run' do
+  it 'fails to run if no test file specified' do
     task_result = test_run_task('test::role', 'test_tool' => 'inspec')
     expect(task_result[0]['status']).to eq('failure')
   end
 
-  it 'returns helpful error message' do
+  it 'returns helpful error message if no test file specified' do
     task_result = test_run_task('test::role', 'test_tool' => 'inspec')
+    expect(task_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
+  end
+
+  it 'fails to run if no build tools are present' do
+    task_result = test_run_task('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
+    expect(task_result[0]['status']).to eq('failure')
+  end
+
+  it 'returns helpful error message if no build tools are present' do
+    task_result = test_run_task('test::role', 'test_tool' => 'inspec', 'test_file' => 'example_pass.rb')
     expect(task_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
   end
 end
