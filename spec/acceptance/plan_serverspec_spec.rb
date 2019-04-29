540f84e5ef4aff83ce5fa6dda70765e36a8979f9
davidjnewton <davidnewton82@gmail.com>
2019-04-29 22:52:51 +0100

    lint'

--- spec/acceptance/plan_serverspec_spec.rb
+++ spec/acceptance/plan_serverspec_spec.rb
@@ -5,13 +5,13 @@ describe 'plan test::role with serverspec' do
     plan_result = test_run_plan('test::role',
                                 'test_params' => { 'test_tool' => 'serverspec' },
                                 'ctrl_params' => { 'tmp_dir' => '/tmp' })
-    expect(plan_result['value'][0]['status']).to eq('failure')
+    expect(plan_result).to eq('failure')
   end
   it 'returns helpful error message if no test_file specified' do
     plan_result = test_run_plan('test::role',
                                 'test_params' => { 'test_tool' => 'serverspec' },
                                 'ctrl_params' => { 'tmp_dir' => '/tmp' })
-    expect(plan_result['value'][0]['result']['error']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
+    expect(plan_result).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
   end
   it 'installs (stages) the serverspec gem on the controller' do
     test_run_plan('test::role',
