6cc29d61630fd0e4881d8d28c87abe0dce256fab
davidjnewton <davidnewton82@gmail.com>
2019-04-29 15:57:37 +0100

    specs for ss

--- spec/acceptance/task_serverspec_spec.rb
+++ spec/acceptance/task_serverspec_spec.rb
@@ -11,7 +11,7 @@ describe 'test_tool=serverspec' do
   it 'fails and returns helpful error message when no params are specified' do
     task_params = { 'test_tool' => 'serverspec' }
     task_result = test_run_task('test::role', task_params)
-    expect(task_result[0]['result']['_output']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
+    expect(task_result[0]).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter!})
   end
 
   it 'runs a passing test successfully' do
@@ -23,7 +23,7 @@ describe 'test_tool=serverspec' do
   it 'returns output' do
     task_params = { 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb' }
     task_result = test_run_task('test::role', task_params)
-    expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailurees})
+    expect(task_result[0]['result']['_output']).to match(%r{\d+\sexamples,\s0\sfailures})
   end
 
   it 'fails a failing test' do
