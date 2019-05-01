48a19aa966f23238669869cfc2c31445eeaa954f
davidjnewton <davidnewton82@gmail.com>
2019-05-01 20:55:31 +0100

    error handling

--- spec/acceptance/plan_serverspec_spec.rb
+++ spec/acceptance/plan_serverspec_spec.rb
@@ -6,20 +6,13 @@ describe 'plan test::role with serverspec' do
     plan_result = test_run_plan('test::role',
                                 'test_params' => { 'test_tool' => 'serverspec' },
                                 'ctrl_params' => { 'tmp_dir' => '/tmp' })
-    expect(plan_result).to eq('failure')
+    expect(plan_result['status']).to eq('failure')
   end
   it 'returns helpful error message if no test_file specified' do
     plan_result = test_run_plan('test::role',
                                 'test_params' => { 'test_tool' => 'serverspec' },
                                 'ctrl_params' => { 'tmp_dir' => '/tmp' })
-    expect(plan_result).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
-  end
-  it 'installs (stages) the serverspec gem on the controller' do
-    test_run_plan('test::role',
-                  'test_params' => { 'test_tool' => 'serverspec', 'test_file' => 'example_pass.rb' },
-                  'ctrl_params' => { 'tmp_dir' => '/tmp' })
-    cmd_result = test_run_command('ls /tmp/puppet_test/serverspec/', 'localhost')
-    expect(cmd_result[0]).to eq('success')
+    expect(plan_result['value']).to match(%r{unable\sto\sdetect\sthis\snode.*\srole\susing\sfacter})
   end
   it 'runs and passes a passing test' do
     plan_result = test_run_plan('test::role',
